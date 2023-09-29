bdf4b18ab49f1bb3b56c21bf7450637be043a12c
Chris Williams <chrisfwilliams@gmail.com>
2023-09-29 22:56:45 +0000

    #25 enable static website hosting

--- modules/terrahouse_aws/main.tf
+++ modules/terrahouse_aws/main.tf
@@ -1,18 +1,11 @@
 terraform {
-  #   backend "remote" {
-  #     organization = "Mistwire"
-  #     workspaces {
-  #       name = "terra-house-1"
-  #     }
-  #   }
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "5.17.0"
+      source = "hashicorp/aws"
+      version = "5.16.2"
     }
   }
 }
-
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
 resource "aws_s3_bucket" "website_bucket" {
@@ -21,7 +14,36 @@ resource "aws_s3_bucket" "website_bucket" {
   bucket = var.bucket_name
 
   tags = {
-    UserUuid    = var.user_uuid
-    Environment = "Dev"
+    UserUuid = var.user_uuid
   }
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_website_configuration
+resource "aws_s3_bucket_website_configuration" "website_configuration" {
+  bucket = aws_s3_bucket.website_bucket.bucket
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_object
+resource "aws_s3_object" "index_html" {
+  bucket = aws_s3_bucket.website_bucket.bucket
+  key    = "index.html"
+  source = var.index_html_filepath
+
+  etag = filemd5(var.index_html_filepath)
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_object
+resource "aws_s3_object" "error_html" {
+  bucket = aws_s3_bucket.website_bucket.bucket
+  key    = "error.html"
+  source = var.error_html_filepath
+
+  etag = filemd5(var.error_html_filepath)
 }
--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "bucket_name" {
   description = "Bucket name for our static website hosting"
   value = module.terrahouse_aws.random_bucket_name
 }
+
+output "website_endpoint" {
+  description = "S3 static website endpoint"
+  value = module.terrahouse_aws.website_endpoint
+}
