4349d9093c3cccbdcfbae463a7cb39f16bcde14d
Chris Williams <chrisfwilliams@gmail.com>
2023-09-29 20:38:51 -0400

    #27 CDN implementation (#28)

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "bucket_name" {
   description = "Bucket name for our static website hosting"
-  value = module.terrahouse_aws.random_bucket_name
+  value       = module.terrahouse_aws.random_bucket_name
 }
 
 output "website_endpoint" {
   description = "S3 static website endpoint"
-  value = module.terrahouse_aws.website_endpoint
+  value       = module.terrahouse_aws.website_endpoint
 }
