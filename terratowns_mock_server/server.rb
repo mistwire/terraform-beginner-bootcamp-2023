49387ee730663ce532e064a386585c3db9aa8d02
Chris Williams <chrisfwilliams@gmail.com>
2023-11-01 01:53:42 +0000

    #45 fix endpoints

--- terratowns_mock_server/server.rb
+++ terratowns_mock_server/server.rb
@@ -188,9 +188,9 @@ class TerraTownsMockServer < Sinatra::Base
 
     home = Home.new
     home.town = $home[:town]
+    home.domain_name = $home[:domain_name]
     home.name = name
     home.description = description
-    home.domain_name = domain_name
     home.content_version = content_version
 
     unless home.valid?
